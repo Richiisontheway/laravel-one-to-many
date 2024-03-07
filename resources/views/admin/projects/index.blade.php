71822c0bd667a2480fe0e2702e97a18a269e8066
Richiisontheway <mineiriccardo@gmail.com>
2024-03-07 21:14:09 +0100

    add in projects.index & types.show route

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -23,6 +23,7 @@
                             <tr>
                                 <th scope="col">#</th>
                                 <th scope="col">Titolo</th>
+                                <th scope="col">Tipo</th>
                                 <th scope="col">Date</th>
                                 <th scope="col">Azioni</th>
                             </tr>
@@ -32,6 +33,15 @@
                                 <tr>
                                     <th scope="row">{{$singleAttribute->id}}</th>
                                     <td>{{$singleAttribute->title}}</td>
+                                    <td>
+                                        {{-- pre farlo diventare un link la rotta show ha bisogno di un dato
+                                            id per funzionare e lo prendiamo fando $var->type(che ci prende la funzione dentro il model Project)
+                                            ->per l'id
+                                         --}}
+                                        <a href="{{route('admin.types.show', ['type' => $singleAttribute->type->id]) }}">
+                                            {{$singleAttribute->type->title}}
+                                        </a>
+                                    </td>
                                     <td>{{$singleAttribute->date}}</td>
                                     <td>
                                         <a href="{{ route('admin.projects.show' , ['project' => $singleAttribute->slug]) }}" class="btn btn-primary">
--- resources/views/admin/types/show.blade.php
+++ resources/views/admin/types/show.blade.php
@@ -8,9 +8,23 @@
             <div class="card">
                 <div class="card-body">
                     <h1 class="text-center text-success">
-                        {{$project->title}}
+                        {{$type->title}}
                     </h1>
-                    <br>
+                </div>
+                <div>
+                    <h2 class="text-center">
+                        Tutti i Progetti appartenenti alla categoria: {{$type->title}}
+                    </h2>
+                    <ul>
+                                {{-- prende la funzione dentro il model Type --}}
+                        @foreach ($type->projects as $project)
+                            <li>
+                                <a href="{{route('admin.projects.show', ['project' => $project->slug])}}">
+                                    {{$project->title}}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
                 </div>
             </div>
         </div>
